fdce6d2aeebeb508289a5674614c62b91c99db60
Sean Christopherson <sean.j.christopherson@intel.com>
2018-11-01 18:21:58 -0700

    x86/sgx: Add sgx_einit() for initializing enclaves
    Add a helper function to perform ENCLS(EINIT) with the correct LE
    hash MSR values.  ENCLS[EINIT] initializes an enclave, verifying the
    enclave's measurement and preparing it for execution, i.e. the enclave
    cannot be run until it has been initialized.  The measurement aspect
    of EINIT references the MSR_IA32_SGXLEPUBKEYHASH* MSRs, with the CPU
    comparing CPU compares the key (technically its hash) used to sign the
    enclave[1] with the key hash stored in the MSRs, and will reject EINIT
    if the keys do not match.
    
    A per-cpu cache is used to avoid writing the MSRs as writing the MSRs
    is extraordinarily expensive, e.g. 300-400 cycles per MSR.  Because
    the cache may become stale, force update the MSRs and retry EINIT if
    the first EINIT fails due to an "invalid token".  An invalid token
    error does not necessarily mean the MSRs need to be updated, but the
    cost of an unnecessary write is minimal relative to the cost of EINIT
    itself.
    
    [1] For EINIT's purposes, the effective signer of the enclave may be
        the enclave's owner, or a separate Launch Enclave that has created
        an EINIT token for the target enclave.  When using an EINIT token,
        the key used to sign the token must match the MSRs in order for
        EINIT to succeed.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

--- arch/x86/kernel/cpu/sgx/sgx.h
+++ arch/x86/kernel/cpu/sgx/sgx.h
@@ -67,5 +67,7 @@ int sgx_page_reclaimer_init(void);
 struct sgx_epc_page *sgx_alloc_page(void);
 int __sgx_free_page(struct sgx_epc_page *page);
 void sgx_free_page(struct sgx_epc_page *page);
+int sgx_einit(struct sgx_sigstruct *sigstruct, struct sgx_einittoken *token,
+	      struct sgx_epc_page *secs, u64 *lepubkeyhash);
 
 #endif /* _X86_SGX_H */
