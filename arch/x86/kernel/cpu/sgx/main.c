aa984b6b118d01c8d2d43f1217c8afbec7cc81d3
Sean Christopherson <sean.j.christopherson@intel.com>
2018-11-01 18:21:58 -0700

    x86/sgx: Add sgx_einit() for initializing enclaves
    Add a helper function to perform ENCLS(EINIT) with the correct LE
    hash MSR values.  ENCLS[EINIT] initializes an enclave, verifying the
    enclave's measurement and preparing it for execution, i.e. the enclave
    cannot be run until it has been initialized.  The measurement aspect
    of EINIT references the MSR_IA32_SGXLEPUBKEYHASH* MSRs, with the CPU
    comparing CPU compares the key (technically its hash) used to sign the
    enclave[1] with the key hash stored in the MSRs, and will reject EINIT
    if the keys do not match.
    
    A per-cpu cache is used to avoid writing the MSRs as writing the MSRs
    is extraordinarily expensive, e.g. 300-400 cycles per MSR.  Because
    the cache may become stale, force update the MSRs and retry EINIT if
    the first EINIT fails due to an "invalid token".  An invalid token
    error does not necessarily mean the MSRs need to be updated, but the
    cost of an unnecessary write is minimal relative to the cost of EINIT
    itself.
    
    [1] For EINIT's purposes, the effective signer of the enclave may be
        the enclave's owner, or a separate Launch Enclave that has created
        an EINIT token for the target enclave.  When using an EINIT token,
        the key used to sign the token must match the MSRs in order for
        EINIT to succeed.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

--- arch/x86/kernel/cpu/sgx/main.c
+++ arch/x86/kernel/cpu/sgx/main.c
@@ -17,6 +17,9 @@ EXPORT_SYMBOL_GPL(sgx_epc_sections);
 
 int sgx_nr_epc_sections;
 
+/* A per-cpu cache for the last known values of IA32_SGXLEPUBKEYHASHx MSRs. */
+static DEFINE_PER_CPU(u64 [4], sgx_lepubkeyhash_cache);
+
 static struct sgx_epc_page *sgx_section_get_page(
 	struct sgx_epc_section *section)
 {
@@ -105,6 +108,54 @@ void sgx_free_page(struct sgx_epc_page *page)
 	WARN(ret > 0, "sgx: EREMOVE returned %d (0x%x)", ret, ret);
 }
 EXPORT_SYMBOL_GPL(sgx_free_page);
+
+static void sgx_update_lepubkeyhash_msrs(u64 *lepubkeyhash, bool enforce)
+{
+	u64 *cache;
+	int i;
+
+	cache = per_cpu(sgx_lepubkeyhash_cache, smp_processor_id());
+	for (i = 0; i < 4; i++) {
+		if (enforce || (lepubkeyhash[i] != cache[i])) {
+			wrmsrl(MSR_IA32_SGXLEPUBKEYHASH0 + i, lepubkeyhash[i]);
+			cache[i] = lepubkeyhash[i];
+		}
+	}
+}
+
+/**
+ * sgx_einit - initialize an enclave
+ * @sigstruct:		a pointer a SIGSTRUCT
+ * @token:		a pointer an EINITTOKEN (optional)
+ * @secs:		a pointer a SECS
+ * @lepubkeyhash:	the desired value for IA32_SGXLEPUBKEYHASHx MSRs
+ *
+ * Execute ENCLS[EINIT], writing the IA32_SGXLEPUBKEYHASHx MSRs according
+ * to @lepubkeyhash (if possible and necessary).
+ *
+ * Return:
+ *   0 on success,
+ *   -errno or SGX error on failure
+ */
+int sgx_einit(struct sgx_sigstruct *sigstruct, struct sgx_einittoken *token,
+	      struct sgx_epc_page *secs, u64 *lepubkeyhash)
+{
+	int ret;
+
+	if (!boot_cpu_has(X86_FEATURE_SGX_LC))
+		return __einit(sigstruct, token, sgx_epc_addr(secs));
+
+	preempt_disable();
+	sgx_update_lepubkeyhash_msrs(lepubkeyhash, false);
+	ret = __einit(sigstruct, token, sgx_epc_addr(secs));
+	if (ret == SGX_INVALID_EINITTOKEN) {
+		sgx_update_lepubkeyhash_msrs(lepubkeyhash, true);
+		ret = __einit(sigstruct, token, sgx_epc_addr(secs));
+	}
+	preempt_enable();
+	return ret;
+}
+EXPORT_SYMBOL(sgx_einit);
 
 static __init void sgx_free_epc_section(struct sgx_epc_section *section)
 {
