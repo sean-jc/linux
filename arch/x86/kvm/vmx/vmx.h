f5bb467811ee192eba05060f19b36b3494e6b694
Sean Christopherson <sean.j.christopherson@intel.com>
2020-07-14 12:12:36 -0700

    KVM: nVMX: Explicitly check for valid guest state for !unrestricted guest
    Call guest_state_valid() directly instead of querying emulation_required
    when checking if L1 is attempting VM-Enter with invalid guest state.
    If emulate_invalid_guest_state is false, KVM will fixup segment regs to
    avoid emulation and will never set emulation_required, i.e. KVM will
    incorrectly miss the associated consistency checks because the nested
    path stuffs segments directly into vmcs02.
    
    Opportunsitically add Consistency Check tracing to make future debug
    suck a little less.
    
    Fixes: 2bb8cafea80bf ("KVM: vVMX: signal failure for nested VMEntry if emulation_required")
    Fixes: 3184a995f782c ("KVM: nVMX: fix vmentry failure code when L2 state would require emulation")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -343,6 +343,15 @@ void vmx_get_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg);
 void vmx_set_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg);
 u64 construct_eptp(struct kvm_vcpu *vcpu, unsigned long root_hpa,
 		   int root_level);
+
+bool __vmx_guest_state_valid(struct kvm_vcpu *vcpu);
+static inline bool vmx_guest_state_valid(struct kvm_vcpu *vcpu)
+{
+	if (enable_unrestricted_guest)
+		return true;
+
+	return __vmx_guest_state_valid(vcpu);
+}
 void update_exception_bitmap(struct kvm_vcpu *vcpu);
 void vmx_update_msr_bitmap(struct kvm_vcpu *vcpu);
 bool vmx_nmi_blocked(struct kvm_vcpu *vcpu);
