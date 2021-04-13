ca231544a3208cb6a9e0b9b4ccd5ba8a496e2758
Sean Christopherson <seanjc@google.com>
2021-04-13 10:03:30 -0700

    KVM: x86: Consolidate guest enter/exit logic to common helpers
    Move the enter/exit logic in {svm,vmx}_vcpu_enter_exit() to common
    helpers.  In addition to deduplicating code, this will allow tweaking the
    vtime accounting in the VM-Exit path without splitting logic across x86,
    VMX, and SVM.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.h
+++ arch/x86/kvm/x86.h
@@ -8,6 +8,62 @@
 #include "kvm_cache_regs.h"
 #include "kvm_emulate.h"
 
+static inline void kvm_vtime_account_guest_exit(bool irqs_handled)
+{
+	if (irqs_handled != vtime_accounting_enabled_this_cpu()) {
+		instrumentation_begin();
+		vtime_account_guest_exit();
+		instrumentation_end();
+	}
+}
+
+static __always_inline void kvm_guest_enter_irqoff(void)
+{
+	/*
+	 * VMENTER enables interrupts (host state), but the kernel state is
+	 * interrupts disabled when this is invoked. Also tell RCU about
+	 * it. This is the same logic as for exit_to_user_mode().
+	 *
+	 * This ensures that e.g. latency analysis on the host observes
+	 * guest mode as interrupt enabled.
+	 *
+	 * guest_enter_irqoff() informs context tracking about the
+	 * transition to guest mode and if enabled adjusts RCU state
+	 * accordingly.
+	 */
+	instrumentation_begin();
+	trace_hardirqs_on_prepare();
+	lockdep_hardirqs_on_prepare(CALLER_ADDR0);
+	instrumentation_end();
+
+	guest_enter_irqoff();
+	lockdep_hardirqs_on(CALLER_ADDR0);
+}
+
+static __always_inline void kvm_guest_exit_irqoff(void)
+{
+	/*
+	 * VMEXIT disables interrupts (host state), but tracing and lockdep
+	 * have them in state 'on' as recorded before entering guest mode.
+	 * Same as enter_from_user_mode().
+	 *
+	 * context_tracking_guest_exit_irqoff() restores host context and
+	 * reinstates RCU if enabled and required.
+	 *
+	 * This needs to be done before the below as native_read_msr()
+	 * contains a tracepoint and x86_spec_ctrl_restore_host() calls
+	 * into world and some more.
+	 */
+	lockdep_hardirqs_off(CALLER_ADDR0);
+	context_tracking_guest_exit_irqoff();
+
+	kvm_vtime_account_guest_exit(false);
+
+	instrumentation_begin();
+	trace_hardirqs_off_finish();
+	instrumentation_end();
+}
+
 #define KVM_NESTED_VMENTER_CONSISTENCY_CHECK(consistency_check)		\
 ({									\
 	bool failed = (consistency_check);				\
@@ -400,15 +456,6 @@ static inline void kvm_machine_check(void)
 
 	do_machine_check(&regs);
 #endif
-}
-
-static inline void kvm_vtime_account_guest_exit(bool irqs_handled)
-{
-	if (irqs_handled != vtime_accounting_enabled_this_cpu()) {
-		instrumentation_begin();
-		vtime_account_guest_exit();
-		instrumentation_end();
-	}
 }
 
 void kvm_load_guest_xsave_state(struct kvm_vcpu *vcpu);
