396cd29a222c9eb9c142242a6db2e148ad21c6d9
Sean Christopherson <seanjc@google.com>
2021-08-10 10:22:33 -0700

    KVM: x86/mmu: Do not allow shadow pages to become unsync under TDP MMU
    Fixes: a2855afc7ee8 ("KVM: x86/mmu: Allow parallel page faults for the TDP MMU")
    Cc: Ben Gardon <bgardon@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -950,7 +950,7 @@ static int tdp_mmu_map_handle_target_level(struct kvm_vcpu *vcpu, int write,
 		new_spte = make_mmio_spte(vcpu, iter->gfn, ACC_ALL);
 	else
 		make_spte_ret = make_spte(vcpu, ACC_ALL, iter->level, iter->gfn,
-					 pfn, iter->old_spte, prefault, true,
+					 pfn, iter->old_spte, prefault, false,
 					 map_writable, !shadow_accessed_mask,
 					 &new_spte);
 
