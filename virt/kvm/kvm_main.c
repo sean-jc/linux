e8bbc285add95cdf0ae363f2a8b76e736c2c4150
Jing Zhang <jingzhangos@google.com>
2021-10-08 19:12:08 -0700

    KVM: stats: Add stat to detect if vcpu is currently blocking
    Add a "blocking" stat that userspace can use to detect the case where a
    vCPU is not being run because of an vCPU/guest action, e.g. HLT or WFS on
    x86, WFI on arm64, etc...  Current guest/host/halt stats don't show this
    well, e.g. if a guest halts for a long period of time then the vCPU could
    could appear pathologically blocked due to a host condition, when in
    reality the vCPU has been put into a not-runnable state by the guest.
    
    Originally-by: Cannon Matthews <cannonmatthews@google.com>
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: David Matlack <dmatlack@google.com>
    Signed-off-by: Jing Zhang <jingzhangos@google.com>
    [sean: renamed stat to "blocking", massaged changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20211009021236.4122790-16-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3166,6 +3166,8 @@ bool kvm_vcpu_block(struct kvm_vcpu *vcpu)
 	struct rcuwait *wait = kvm_arch_vcpu_get_wait(vcpu);
 	bool waited = false;
 
+	vcpu->stat.generic.blocking = 1;
+
 	kvm_arch_vcpu_blocking(vcpu);
 
 	prepare_to_rcuwait(wait);
@@ -3181,6 +3183,8 @@ bool kvm_vcpu_block(struct kvm_vcpu *vcpu)
 	finish_rcuwait(wait);
 
 	kvm_arch_vcpu_unblocking(vcpu);
+
+	vcpu->stat.generic.blocking = 0;
 
 	return waited;
 }
