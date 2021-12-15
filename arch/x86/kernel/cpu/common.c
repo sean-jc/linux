1a82034a2e65448beadc2a7cc365efc496fa4d58
David Woodhouse <dwmw@amazon.co.uk>
2021-12-15 14:56:33 +0000

    x86/smpboot: Serialize topology updates for secondary bringup
    If we bring up secondaries in parallel they might get confused unless we
    impose some ordering here:
    
    [    1.360149] x86: Booting SMP configuration:
    [    1.360221] .... node  #0, CPUs:        #1  #2  #3  #4  #5  #6  #7  #8  #9 #10 #11 #12 #13 #14 #15 #16 #17 #18 #19 #20 #21 #22 #23
    [    1.366225] .... node  #1, CPUs:   #24 #25 #26 #27 #28 #29 #30 #31 #32 #33 #34 #35 #36 #37 #38 #39 #40 #41 #42 #43 #44 #45 #46 #47
    [    1.370219] .... node  #0, CPUs:   #48 #49 #50 #51 #52 #53 #54 #55 #56 #57 #58 #59 #60 #61 #62 #63 #64 #65 #66 #67 #68 #69 #70 #71
    [    1.378226] .... node  #1, CPUs:   #72 #73 #74 #75 #76 #77 #78 #79 #80 #81 #82 #83 #84 #85 #86 #87 #88 #89 #90 #91 #92 #93 #94 #95
    [    1.382037] Brought 96 CPUs to x86/cpu:kick in 72232606 cycles
    [    0.104104] smpboot: CPU 26 Converting physical 0 to logical die 1
    [    0.104104] smpboot: CPU 27 Converting physical 1 to logical package 2
    [    0.104104] smpboot: CPU 24 Converting physical 1 to logical package 3
    [    0.104104] smpboot: CPU 27 Converting physical 0 to logical die 2
    [    0.104104] smpboot: CPU 25 Converting physical 1 to logical package 4
    [    1.385609] Brought 96 CPUs to x86/cpu:wait-init in 9269218 cycles
    [    1.395285] Brought CPUs online in 28930764 cycles
    [    1.395469] smp: Brought up 2 nodes, 96 CPUs
    [    1.395689] smpboot: Max logical packages: 2
    [    1.396222] smpboot: Total of 96 processors activated (576000.00 BogoMIPS)
    
    Do the full topology update in smp_store_cpu_info() under a spinlock
    to ensure that things remain consistent.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1517,7 +1517,7 @@ static void generic_identify(struct cpuinfo_x86 *c)
  * Validate that ACPI/mptables have the same information about the
  * effective APIC id and update the package map.
  */
-static void validate_apic_and_package_id(struct cpuinfo_x86 *c)
+static void validate_apic_id(struct cpuinfo_x86 *c)
 {
 #ifdef CONFIG_SMP
 	unsigned int apicid, cpu = smp_processor_id();
@@ -1528,8 +1528,6 @@ static void validate_apic_and_package_id(struct cpuinfo_x86 *c)
 		pr_err(FW_BUG "CPU%u: APIC id mismatch. Firmware: %x APIC: %x\n",
 		       cpu, apicid, c->initial_apicid);
 	}
-	BUG_ON(topology_update_package_map(c->phys_proc_id, cpu));
-	BUG_ON(topology_update_die_map(c->cpu_die_id, cpu));
 #else
 	c->logical_proc_id = 0;
 #endif
@@ -1716,7 +1714,7 @@ void identify_secondary_cpu(struct cpuinfo_x86 *c)
 	enable_sep_cpu();
 #endif
 	mtrr_ap_init();
-	validate_apic_and_package_id(c);
+	validate_apic_id(c);
 	x86_spec_ctrl_setup_ap();
 	update_srbds_msr();
 }
