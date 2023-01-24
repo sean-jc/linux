0efc5a446888804eaa76a377fb017f8eea995e79
Kim Phillips <kim.phillips@amd.com>
2023-01-24 10:33:19 -0600

    KVM: x86: Propagate the AMD Automatic IBRS feature to the guest
    Add the AMD Automatic IBRS feature bit to those being propagated to the guest,
    and enable the guest EFER bit.
    
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -743,7 +743,7 @@ void kvm_set_cpu_caps(void)
 
 	kvm_cpu_cap_mask(CPUID_8000_0021_EAX,
 		F(NO_NESTED_DATA_BP) | F(LFENCE_RDTSC) | 0 /* SmmPgCfgLock */ |
-		F(NULL_SEL_CLR_BASE) | 0 /* PrefetchCtlMsr */
+		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */
 	);
 	if (!static_cpu_has_bug(X86_BUG_NULL_SEG))
 		kvm_cpu_cap_set(X86_FEATURE_NULL_SEL_CLR_BASE);
