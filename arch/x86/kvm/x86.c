7054b0ca86370a31e296fb7d724159a8add81c92
Sean Christopherson <seanjc@google.com>
2024-10-28 14:59:34 -0700

    KVM: x86: Drop superfluous "has assigned device" check in kvm_pi_update_irte()
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -13557,8 +13557,7 @@ static int kvm_pi_update_irte(struct kvm_kernel_irqfd *irqfd,
 	struct kvm_lapic_irq irq;
 	int r;
 
-	if (!irqchip_in_kernel(kvm) ||
-	    !kvm_arch_has_assigned_device(kvm))
+	if (!irqchip_in_kernel(kvm))
 		return 0;
 
 	if (new && new->type == KVM_IRQ_ROUTING_MSI) {
