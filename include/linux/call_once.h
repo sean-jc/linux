e1caa221393596218cd99f4c2d5d00400f6901d6
Sean Christopherson <seanjc@google.com>
2025-02-27 15:06:31 -0800

    KVM: x86/mmu: Allow retry of nx_huge_page_recovery_thread creation
    A VMM may send a non-fatal signal to its threads, including vCPU tasks,
    at any time, and thus may signal vCPU tasks during KVM_RUN.  If a vCPU
    task receives the signal while its trying to spawn the huge page recovery
    vhost task, then KVM_RUN will fail due to copy_process() returning
    -ERESTARTNOINTR.
    
    Rework call_once() to mark the call complete if and only if the called
    function succeeds, and plumb the function's true error code back to the
    call_once() invoker.  This provides userspace with the correct, non-fatal
    error code so that the VMM doesn't terminate the VM on -ENOMEM, and allows
    subsequent KVM_RUN a succeed by virtue of retrying creation of the NX huge
    page task.
    
    Opportunistically replace spaces with tabs in call_once.h.
    
    Fixes: 931656b9e2ff ("kvm: defer huge page recovery vhost task to later")
    Co-developed-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/call_once.h
+++ include/linux/call_once.h
@@ -9,15 +9,15 @@
 #define ONCE_COMPLETED   2
 
 struct once {
-        atomic_t state;
-        struct mutex lock;
+	atomic_t state;
+	struct mutex lock;
 };
 
 static inline void __once_init(struct once *once, const char *name,
 			       struct lock_class_key *key)
 {
-        atomic_set(&once->state, ONCE_NOT_STARTED);
-        __mutex_init(&once->lock, name, key);
+	atomic_set(&once->state, ONCE_NOT_STARTED);
+	__mutex_init(&once->lock, name, key);
 }
 
 #define once_init(once)							\
@@ -26,20 +26,26 @@ do {									\
 	__once_init((once), #once, &__key);				\
 } while (0)
 
-static inline void call_once(struct once *once, void (*cb)(struct once *))
+static inline int call_once(struct once *once, int (*cb)(struct once *))
 {
-        /* Pairs with atomic_set_release() below.  */
-        if (atomic_read_acquire(&once->state) == ONCE_COMPLETED)
-                return;
+	int r, state;
 
-        guard(mutex)(&once->lock);
-        WARN_ON(atomic_read(&once->state) == ONCE_RUNNING);
-        if (atomic_read(&once->state) != ONCE_NOT_STARTED)
-                return;
+	/* Pairs with atomic_set_release() below.  */
+	if (atomic_read_acquire(&once->state) == ONCE_COMPLETED)
+		return 0;
 
-        atomic_set(&once->state, ONCE_RUNNING);
-        cb(once);
-        atomic_set_release(&once->state, ONCE_COMPLETED);
+	guard(mutex)(&once->lock);
+	state = atomic_read(&once->state);
+	if (unlikely(state != ONCE_NOT_STARTED))
+		return WARN_ON_ONCE(state != ONCE_COMPLETED) ? -EINVAL : 0;
+
+	atomic_set(&once->state, ONCE_RUNNING);
+	r = cb(once);
+	if (r)
+		atomic_set(&once->state, ONCE_NOT_STARTED);
+	else
+		atomic_set_release(&once->state, ONCE_COMPLETED);
+	return r;
 }
 
 #endif /* _LINUX_CALL_ONCE_H */
