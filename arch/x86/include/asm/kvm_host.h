4eb77db4a14f67bda7edecec8caf975f48a99bc2
Sean Christopherson <seanjc@google.com>
2025-03-05 09:27:35 -0800

    KVM: x86/mmu: Dynamically allocate shadow MMU's hashed page list
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1411,7 +1411,7 @@ struct kvm_arch {
 	u8 mmu_valid_gen;
 
 	unsigned long n_used_mmu_pages;
-	struct hlist_head mmu_page_hash[KVM_NUM_MMU_PAGES];
+	struct hlist_head *mmu_page_hash;
 	struct list_head active_mmu_pages;
 	/*
 	 * A list of kvm_mmu_page structs that, if zapped, could possibly be
@@ -1981,7 +1981,7 @@ void kvm_mmu_vendor_module_exit(void);
 
 void kvm_mmu_destroy(struct kvm_vcpu *vcpu);
 int kvm_mmu_create(struct kvm_vcpu *vcpu);
-void kvm_mmu_init_vm(struct kvm *kvm);
+int kvm_mmu_init_vm(struct kvm *kvm);
 void kvm_mmu_uninit_vm(struct kvm *kvm);
 
 void kvm_mmu_init_memslot_memory_attributes(struct kvm *kvm,
