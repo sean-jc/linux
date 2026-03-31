1ea462fabbc586c35ec9d567aed444c6637f1566
Vipin Sharma <vipinsh@google.com>
2026-03-31 12:41:59 -0700

    KVM: selftests: Add various print flags to KVM selftest runner
    Add various print flags to selectively print outputs on terminal based
    on test execution status (passed, failed, timed out, skipped, no run).
    
    For each status provide further options (off, full, stderr, stdout,
    status) to choose verbosity of their prints. Make "full" the default
    choice.
    
    Example: To print stderr for the failed tests and only status for the
    passed test:
    
       python3 runner --test-dirs tests  --print-failed stderr \
       --print-passed status
    
    Above command with disable print off skipped, timed out, and no run
    tests.
    
    Signed-off-by: Vipin Sharma <vipinsh@google.com>
    [sean: don't print stderr/stdout qualifiers for targeted output]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- tools/testing/selftests/kvm/runner/test_runner.py
+++ tools/testing/selftests/kvm/runner/test_runner.py
@@ -17,6 +17,14 @@ class TestRunner:
         self.tests = []
         self.output_dir = args.output
         self.jobs = args.jobs
+        self.print_stds = {
+            SelftestStatus.PASSED: args.print_passed,
+            SelftestStatus.FAILED: args.print_failed,
+            SelftestStatus.SEGFAULT: args.print_failed,
+            SelftestStatus.SKIPPED: args.print_skipped,
+            SelftestStatus.TIMED_OUT: args.print_timed_out,
+            SelftestStatus.NO_RUN: args.print_no_run
+        }
 
         for testcase in testcases:
             self.tests.append(Selftest(testcase, args.path, args.timeout,
@@ -27,10 +35,18 @@ class TestRunner:
         return test
 
     def _log_result(self, test_result):
-        logger.info("*** stdout ***\n" + test_result.stdout)
-        logger.info("*** stderr ***\n" + test_result.stderr)
-        logger.log(test_result.status,
-                   f"[{test_result.status.name}] {test_result.test_path}")
+        print_level = self.print_stds.get(test_result.status, "full")
+
+        if (print_level == "full"):
+            logger.info("*** stdout ***\n" + test_result.stdout)
+            logger.info("*** stderr ***\n" + test_result.stderr)
+        elif (print_level == "stdout"):
+            logger.info(test_result.stdout)
+        elif (print_level == "stderr"):
+            logger.info(test_result.stderr)
+
+        if (print_level != "off"):
+            logger.log(test_result.status, f"[{test_result.status.name}] {test_result.test_path}")
 
     def start(self):
         ret = 0
