61d65a54aa12c19457566e747e5ad6670797c3e1
David Woodhouse <dwmw@amazon.co.uk>
2026-06-09 19:31:20 +0100

    KVM: selftests: Use UAPI pvclock-abi.h in xen_shinfo_test
    Now that pvclock-abi.h is a UAPI header, use it in xen_shinfo_test.c
    instead of locally duplicating the struct definitions.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>

--- tools/testing/selftests/kvm/x86/xen_shinfo_test.c
+++ tools/testing/selftests/kvm/x86/xen_shinfo_test.c
@@ -98,22 +98,7 @@ struct sched_poll {
 	u64 timeout;
 };
 
-struct pvclock_vcpu_time_info {
-	u32   version;
-	u32   pad0;
-	u64   tsc_timestamp;
-	u64   system_time;
-	u32   tsc_to_system_mul;
-	s8    tsc_shift;
-	u8    flags;
-	u8    pad[2];
-} __attribute__((__packed__)); /* 32 bytes */
-
-struct pvclock_wall_clock {
-	u32   version;
-	u32   sec;
-	u32   nsec;
-} __attribute__((__packed__));
+#include <asm/pvclock-abi.h>
 
 struct vcpu_runstate_info {
 	u32 state;
