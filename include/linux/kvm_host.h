bc91a10f52d50526326d111e08223e7653bda71d
Sean Christopherson <seanjc@google.com>
2026-09-10 13:34:49 -0700

    KVM: WARN if KVM attempts to do guest-related uaccess with "wrong" process
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1352,7 +1352,7 @@ int kvm_gfn_to_hva_cache_init(struct kvm *kvm, struct gfn_to_hva_cache *ghc,
 
 static __always_inline __must_check bool kvm_can_do_uaccess(struct kvm *kvm)
 {
-	return current->mm == kvm->mm;
+	return !WARN_ON_ONCE(current->mm != kvm->mm);
 }
 
 #define BUILD_KVM_COPY_USER_WRAPPER(fn, to_user, from_user)				\
